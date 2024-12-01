eef808d62d9257a5b73230b0a46347fd09263d96
Alyssa Leliveld <alyssaleliveld@outlook.com>
2024-12-01 14:23:45 +0100

    day1

---
+++ 2024/Day1.cs
@@ -0,0 +1,54 @@
+﻿namespace _2024;
+
+public class Day1
+{
+    public void Part1()
+    {
+        int total = 0;
+        (List<int>, List<int>) splitlines = split();
+        var left = splitlines.Item1;
+        var right = splitlines.Item2;
+        
+        for (int i = 0; i < left.Count; i++)
+        {
+            total += Math.Abs(left[i] - right[i]);
+        }
+        
+        Console.WriteLine(total);
+    }
+
+    public void Part2()
+    {
+        int count = 0;
+        int total = 0;
+        (List<int>, List<int>) splitlines = split();
+        var left = splitlines.Item1;
+        var right = splitlines.Item2;
+
+        foreach (var num in left)
+        {
+            count = right.Where(number => number == num).ToList().Count;
+            total += num * count;
+        }
+        
+        Console.WriteLine(total);
+    }
+
+    private (List<int>, List<int>) split()
+    {
+        string[] lines = File.ReadAllLines("Inputs/Input1.txt");
+        List<int> locationsLeft = new List<int>(); 
+        List<int> locationsRight = new List<int>();
+        foreach (string line in lines)
+        {
+            var splitline  = line.Split("   ");
+            locationsLeft.Add(int.Parse(splitline[0]));
+            locationsRight.Add(int.Parse(splitline[1]));
+        }
+        locationsLeft.Sort();
+        locationsRight.Sort();
+        return (locationsLeft, locationsRight);
+    }
+    
+}
+
