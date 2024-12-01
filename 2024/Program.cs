f99894689a9888c95dd54e4c1f87cd0f4aecd3ab
Alyssa Leliveld <alyssaleliveld@outlook.com>
2024-12-01 14:26:18 +0100

    Day 1

---
+++ 2024/Program.cs
@@ -0,0 +1,10 @@
+﻿using _2024;
+
+//Day 1
+Console.WriteLine("Day 1: Advent of Code");
+Day1 day1 = new Day1();
+Console.WriteLine("Part 1:");
+day1.Part1();
+
+Console.WriteLine("Part 2:");
+day1.Part2();
