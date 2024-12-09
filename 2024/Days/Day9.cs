dc6c4488a36d9145d5c51bf4a1a54f8965072d15
Alyssa Leliveld <alyssaleliveld@outlook.com>
2024-12-09 15:05:16 +0100

    Day9

---
+++ 2024/Days/Day9.cs
@@ -0,0 +1,143 @@
+﻿namespace _2024.Days;
+
+public class Day9
+{
+    private readonly List<char> input;
+    private readonly int length;
+
+    public Day9()
+    {
+        input = File.ReadAllText("Inputs/InputDay9.txt").ToCharArray().ToList();
+        length = input.Count;
+    }
+
+    public void Part1()
+    {
+        var disk = BuildDisk(input, length);
+        SwapDisk(disk);
+        Console.WriteLine(CalculateChecksum(disk));
+    }
+
+    private List<char> BuildDisk(List<char> input, int length)
+    {
+        var disk = new List<char>();
+        char idFile = '0';
+
+        for (int i = 0; i < length; i++)
+        {
+            int count = int.Parse(input[i].ToString());
+
+            char toAdd = i % 2 == 0 ? idFile : '.';
+            disk.AddRange(Enumerable.Repeat(toAdd, count));
+
+            if (i % 2 == 0 && count > 0)
+                idFile++;
+        }
+
+        return disk;
+    }
+
+    private void SwapDisk(List<char> disk)
+    {
+        for (int i = 0; i < disk.Count; i++)
+        {
+            if (disk[i] == '.')
+            {
+                for (int j = disk.Count - 1; j > i; j--)
+                {
+                    if (disk[j] != '.')
+                    {
+                        disk[i] = disk[j];
+                        disk[j] = '.';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private long CalculateChecksum(List<char> disk)
+    {
+        long sum = 0;
+
+        for (int position = 0; position < disk.Count; position++)
+        {
+            if (disk[position] != '.')
+            {
+                sum += position * (disk[position] - '0');
+            }
+        }
+
+        return sum;
+    }
+
+
+    public void Part2()
+    {
+        var disk = BuildDisk(input, length);
+        SwapWholeDisk(disk);
+        Console.WriteLine(CalculateChecksum(disk));
+    }
+
+    private void SwapWholeDisk(List<char> disk)
+    {
+        var files = new List<(char id, int start, int length)>();
+        int i = 0;
+
+        while (i < disk.Count)
+        {
+            if (disk[i] != '.')
+            {
+                char id = disk[i];
+                int start = i;
+                
+                while (i < disk.Count && disk[i] == id)
+                    i++;
+
+                files.Add((id, start, i - start)); 
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+        files.Sort((a, b) => b.id.CompareTo(a.id)); 
+
+        foreach (var file in files)
+        {
+            int targetStart = -1;
+            int freeLength = 0;
+
+            for (i = 0; i < disk.Count; i++)
+            {
+                if (disk[i] == '.')
+                {
+                    if (freeLength == 0) 
+                        targetStart = i;
+
+                    freeLength++;
+
+                    if (freeLength >= file.length)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    freeLength = 0; 
+                }
+            }
+            
+            if (freeLength >= file.length && targetStart != -1 && targetStart < file.start)
+            {
+                for (i = file.start; i < file.start + file.length; i++)
+                    disk[i] = '.';
+                
+                for (i = targetStart; i < targetStart + file.length; i++)
+                    disk[i] = file.id;
+            }
+        }
+    }
+
+}
--- 2024/Program.cs
+++ 2024/Program.cs
@@ -2,76 +2,85 @@
 using _2024.Days;
 
 //Day 1
-Console.WriteLine("Day 1: Advent of Code");
+/*Console.WriteLine("Day 1: Advent of Code");
 Console.Write("Part 1: ");
 Day1 day1 = new Day1();
 day1.Part1();
 
 Console.Write("Part 2: ");
-day1.Part2();
+day1.Part2();*/
 
 //Day 2
-Console.WriteLine("\nDay 2: Advent of Code");
+/*Console.WriteLine("\nDay 2: Advent of Code");
 Console.Write("Part 1: ");
 Day2 day2 = new Day2();
 day2.Part1();
 
 Console.Write("Part 2: ");
-day2.Part2();
+day2.Part2();*/
 
 //Day 3
-Console.WriteLine("\nDay 3: Advent of Code");
+/*Console.WriteLine("\nDay 3: Advent of Code");
 Console.Write("Part 1: ");
 Day3 day3 = new Day3();
 day3.Part1();
 
 Console.Write("Part 2: ");
-day3.Part2();
+day3.Part2();*/
 
 //Day 4
-Console.WriteLine("\nDay 4: Advent of Code");
+/*Console.WriteLine("\nDay 4: Advent of Code");
 Console.Write("Part 1: ");
 Day4 day4 = new Day4();
 day4.Part1();
 
 Console.Write("Part 2: ");
-day4.Part2();
+day4.Part2();*/
 
 //Day 5
-Console.WriteLine("\nDay 5: Advent of Code");
+/*Console.WriteLine("\nDay 5: Advent of Code");
 Console.Write("Part 1: ");
 Day5 day5 = new Day5();
 day5.Part1();
 
 Console.Write("Part 2: ");
-day5.Part2();
+day5.Part2();*/
 
 //Day 6
-Console.WriteLine("\nDay 6: Advent of Code");
+/*Console.WriteLine("\nDay 6: Advent of Code");
 Console.Write("Part 1: ");
 Day6 day6 = new Day6();
 day6.Part1();
 
-/*Console.Write("Part 2: ");
+Console.Write("Part 2: ");
 day6.Part2();*/
 
 //Day 7
-Console.WriteLine("\nDay 7: Advent of Code");
+/*Console.WriteLine("\nDay 7: Advent of Code");
 Console.Write("Part 1: ");
 Day7 day7 = new Day7();
 day7.Part1();
 
 Console.Write("Part 2: ");
-day7.Part2();
+day7.Part2();*/
 
 //Day 8
-Console.WriteLine("\nDay 8: Advent of Code");
+/*Console.WriteLine("\nDay 8: Advent of Code");
 Console.Write("Part 1: ");
 Day8 day8 = new Day8();
 day8.Part1();
 
 Console.Write("Part 2: ");
-day8.Part2();
+day8.Part2();*/
+
+//Day 9
+Console.WriteLine("\nDay 9: Advent of Code");
+Console.Write("Part 1: ");
+Day9 day9 = new Day9();
+day9.Part1();
+
+Console.Write("Part 2: ");
+day9.Part2();
 
 //Day 
 /*Console.WriteLine("\nDay : Advent of Code");
